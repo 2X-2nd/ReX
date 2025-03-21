69b9e4e5e3f313a257c44f30706ec600e07a1c41
Sandy Chu <54657610+Pizza3368@users.noreply.github.com>
2025-03-21 14:39:58 -0700

    Update server.js
    fix secruity issue

--- backend/user-service/server.js
+++ backend/user-service/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import 'dotenv/config';
+import express from 'express';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json()); // Allows JSON body parsing
 
-// Import user routes
-const userRoutes = require('./routes/userRoutes');
+// Routes
 app.use('/users', userRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
+
